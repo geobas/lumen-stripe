34580d6327685dce5b36c75f784df4eff08cc1e8
geobas <ksenera@yahoo.com>
2021-10-13 11:33:47 +0300

    create users table

---
+++ database/migrations/2021_10_13_083126_create_users_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('username')->unique();
+            $table->string('password');
+            $table->string('stripe_id')->nullable()->index()->after('name');
+            $table->string('pm_type')->nullable()->after('stripe_id');
+            $table->string('pm_last_four', 4)->nullable()->after('pm_type');
+            $table->timestamp('trial_ends_at')->nullable()->after('pm_last_four');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
