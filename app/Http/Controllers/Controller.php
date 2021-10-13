4dc9ecfde3b9b2502205a5bb6c5d5426f254c5f6
geobas <ksenera@yahoo.com>
2021-10-13 15:33:41 +0300

    add base controller class

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,9 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use Throwable;
+use Illuminate\Support\Facades\Log;
 use Laravel\Lumen\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    //
+    /**
+     * Log error.
+     *
+     * @param  Throwable  $t
+     * @return void
+     *
+     * @throws \Exception|\Error
+     */
+    protected function logError(Throwable $t): void
+    {
+        Log::error("'" . $t->getMessage() . "' - File: '" . $t->getFile() . "' - Method: '" . 
+            debug_backtrace()[1]['function'] . "' at line: " . $t->getLine());
+
+        throw $t;
+    }
 }
