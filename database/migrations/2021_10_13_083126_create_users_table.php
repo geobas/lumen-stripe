013f75d996c2dccc72616ad1a5e7e1f78b07c50c
geobas <ksenera@yahoo.com>
2021-10-13 11:37:49 +0300

    minor change

--- database/migrations/2021_10_13_083126_create_users_table.php
+++ database/migrations/2021_10_13_083126_create_users_table.php
@@ -17,10 +17,10 @@ class CreateUsersTable extends Migration
             $table->increments('id');
             $table->string('username')->unique();
             $table->string('password');
-            $table->string('stripe_id')->nullable()->index()->after('name');
-            $table->string('pm_type')->nullable()->after('stripe_id');
-            $table->string('pm_last_four', 4)->nullable()->after('pm_type');
-            $table->timestamp('trial_ends_at')->nullable()->after('pm_last_four');
+            $table->string('stripe_id')->nullable()->index();
+            $table->string('pm_type')->nullable();
+            $table->string('pm_last_four', 4)->nullable();
+            $table->timestamp('trial_ends_at')->nullable();
             $table->timestamps();
         });
     }
