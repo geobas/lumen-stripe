21ebe17c350c49c99ed35b1a437b8c4c8618fabf
geobas <ksenera@yahoo.com>
2021-10-13 11:41:34 +0300

    define subscription routes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,20 @@
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+
+/*********************** Routes for Subscription Billing ***********************/
+$router->get('subscriptions/plans', [
+    'as' => 'subscriptions.plans', 'uses' => 'SubscriptionController@plans',
+]);
+
+$router->post('subscriptions/create', [
+    'as' => 'subscriptions.create', 'uses' => 'SubscriptionController@create',
+]);
+
+$router->get('subscriptions/user/plan/{planId}', [
+    'as' => 'subscriptions.user.plan', 'uses' => 'SubscriptionController@subscribedTo',
+]);
+
+$router->get('subscriptions/user/cancel/subscription', [
+    'as' => 'subscriptions.user.cancel.subscription', 'uses' => 'SubscriptionController@cancelSubscription',
+]);
